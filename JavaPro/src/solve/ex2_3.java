707a2c5defab39b9f9ac03ba6e2e945a3c6cf153
young sang lee <yohesa@nate.com>
2021-07-06 08:57:20 +0900

    array

--- JavaPro/src/solve/ex2_3.java
+++ JavaPro/src/solve/ex2_3.java
@@ -4,20 +4,5 @@ public class ex2_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[][] arr = new int[10][];
-		arr[0] = new int[] {0,0,0,0,0,0,0,01};
-		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<arr[i].length; j++;) {
-				if(i<=j) {
-					
-				}
-			}
-			
-		}
-		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<arr[i].length; j++) {
-				System.out.println("arr["+i+"]["+j+"]"+arr[i][j]+ " ");
-		}
-	}
 	}
 }
